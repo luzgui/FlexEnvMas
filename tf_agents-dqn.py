05813bc335e19a8ab47ba67af916e23d12ddc3f6
luzgui <gpluz@fc.ul.pt>
2022-01-14 12:16:57 +0000

    add tf-agents-dqn

---
+++ tf_agents-dqn.py
@@ -0,0 +1,50 @@
+"""
+Created on Thu Jan 13 16:48:57 2022
+
+@author: GuiLuz
+"""
+
+from __future__ import absolute_import, division, print_function
+import base64
+import imageio
+import IPython
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL.Image
+import pyvirtualdisplay
+import reverb
+import tensorflow as tf
+
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import py_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import sequential
+from tf_agents.policies import py_tf_eager_policy
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import reverb_replay_buffer
+from tf_agents.replay_buffers import reverb_utils
+from tf_agents.trajectories import trajectory
+from tf_agents.specs import tensor_spec
+from tf_agents.utils import common
+
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import numpy.random as rnd
+import time
+import random as rnd
+
+from time import perf_counter
+
+import gym
+import Antonio_FlexEnv as flex
+
+import Antonio_auxfunctions as fun
+
+
+
