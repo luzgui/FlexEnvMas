1d22c97bbe8b128a770e45c6460d857d077e7532
luzgui <gpluz@fc.ul.pt>
2024-11-12 18:50:05 +0000

    add simple reward

--- reward.py
+++ reward.py
@@ -9,7 +9,8 @@ class Reward:
             
         self.reward_func_list = {
               'sigma_reward': self.coop_sigma_reward,
-              'tar_sigma_reward':self.tar_sigma_reward}
+              'tar_sigma_reward':self.tar_sigma_reward,
+              'simple_reward':self.simple_reward}
     
         self.reward_func=self.reward_func_list[self.self_env.com.scenarios_conf['reward_func']]
 
@@ -43,6 +44,14 @@ class Reward:
     
 
     
+    def exponent(self,x):
+        a=10
+        b=1
+        c=-2.3
+        d=8.3
+        
+        return (a/(b+np.exp(x+c)))+d
+
     def custom_elu(self,x, c):
         # a=0.3
         # b=10
@@ -123,7 +132,44 @@ class Reward:
             
             
             
-            df.loc[ag,'new_r']=df.loc[ag,'alpha_cost']*self.line(df.loc[ag,'d_tar'],2,-1)*self.beta(df.loc[ag,'x_ratio'])+self.indicator(action)*self.custom_elu(df.loc[ag,'alpha_cost'],df.loc[ag,'c_min'])
+            df.loc[ag,'new_r']=df.loc[ag,'alpha_cost']*self.line(df.loc[ag,'d_tar'],2,-1)*self.exponent(df.loc[ag,'x_ratio'])+self.indicator(action)*self.custom_elu(df.loc[ag,'alpha_cost'],df.loc[ag,'c_min'])
+            
+        R=df['new_r'].sum()
+        print(df)
+        return {aid: R for aid in self.self_env.agents_id} 
+    
+    
+    
+    
+    def simple_reward(self):
+        
+        df=self.self_env.action.copy()
+
+        AgentTotalLoads=sum([self.self_env.action.loc[agent]['action']*self.self_env.com.agents[agent].apps[0].base_load*(self.self_env.tstep_size/60) for agent in self.self_env.agents_id])
+        
+        
+        for ag in self.self_env.agents_id:
+            action=self.self_env.action.loc[ag, 'action']
+            base_load=self.self_env.com.agents[ag].apps[0].base_load
+            df.loc[ag,'tar_min']=min(self.self_env.com.agents[ag].make_tariff())
+            df.loc[ag,'tar_max']=max(self.self_env.com.agents[ag].make_tariff())
+            df.loc[ag,'c_min']=-df.loc[ag,'tar_min']*base_load*(self.self_env.tstep_size / 60)
+            
+            if AgentTotalLoads != 0:
+                df.loc[ag, 'alpha'] = (action*base_load*(self.self_env.tstep_size / 60) / AgentTotalLoads)
+            else:
+                df.loc[ag, 'alpha'] = 0  # or handle it in another appropriate way
+
+            df.loc[ag,'alpha_cost']=self.get_agent_cost_alpha(ag, df.loc[ag,'alpha'])
+            
+            df.loc[ag,'d_tar']=(self.self_env.state.loc[ag]['tar_buy']-df.loc[ag,'tar_min'])/(df.loc[ag,'tar_max']-df.loc[ag,'tar_min'])
+            df.loc[ag,'x_ratio']=self.self_env.state.loc[ag]['pv_sum']/self.self_env.agents_params['E_prof'].sum()
+            
+            
+            
+            
+            
+            df.loc[ag,'new_r']=(df.loc[ag,'alpha_cost']+0.1)*self.indicator(action)
             
         R=df['new_r'].sum()
         # print(df)
