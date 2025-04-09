cf8e8ccb2e4b130254bc984358981901bbc69fce
luzgui <gpluz@fc.ul.pt>
2025-04-09 10:18:49 +0100

    add new fixed test class (allows to fix the schedulling)

--- testenv.py
+++ testenv.py
@@ -89,7 +89,7 @@ class TestEnv():
             
                     
             if self.shouldPlot: 
-                self.plot.makeplot_bar(env_state, None)
+                self.plot.makeplot_bar_simple(env_state, None)
                 # self.plot.plot_tarifs_lines(env_state_conc)
                 
                 
@@ -181,9 +181,9 @@ class SimpleTestEnv(TestEnv):
     def get_action_plan(self):
         "fixed action plan"
         actions={}
-        starts=dict(zip(self.env.agents_id, [43,49,49,32,36,45,50][0:len(self.env.agents_id)]))
-        # starts=dict(zip(self.env.agents_id, [47,43,49,32,36,45,50][0:len(self.env.agents_id)]))
-        # starts=dict(zip(self.env.agents_id, [0,0,49,32,36,45,50][0:len(self.env.agents_id)]))
+        # starts=dict(zip(self.env.agents_id, [43,49,49,32,36,45,50][0:len(self.env.agents_id)]))
+        starts=dict(zip(self.env.agents_id, [81,43,49,32,36,45,50][0:len(self.env.agents_id)]))
+        # starts=dict(zip(self.env.agents_id, [44,0,49,32,36,45,50][0:len(self.env.agents_id)]))
         # starts=dict(zip(self.env.agents_id, [78,78,85,36,45,50][0:len(self.env.agents_id)]))
         
         # starts=dict(zip(self.env.agents_id, [40,46,85,36,45,50][0:len(self.env.agents_id)]))
@@ -275,6 +275,49 @@ class BaselineTest(SimpleTestEnv):
             actions[ag]=self.create_binary_vector(self.env.Tw,D,starts[ag])
         
         return actions
+
+
+
+class BaselineFixedTest(TestEnv):
+    "Implements agents that start at a fixed timeslots during the day"
+    def __init__(self,env, tester, file_experiment,test_config_file):
+        super().__init__(env, tester, file_experiment,test_config_file)
+        self.counter=0
+        self.action_plan=self.get_action_plan()
+    
+    
+    def get_actions(self, obs, map_func):
+        "Overrides the superclass methods"   
+        actions = {aid: self.action_plan[aid][self.counter] for aid in self.env.agents_id}
+        
+        if self.counter <= self.env.Tw-2:
+            self.counter+=1
+        else:
+            self.action_plan=self.get_action_plan()
+            self.counter=0
+            
+        return actions
+    
+    def create_binary_vector(self, T, D, t):
+        """
+        Create a binary vector with zeros everywhere except for a specific duration D starting at time t.
+
+        """
+        binary_vector = np.zeros(T, dtype=int)
+        binary_vector[t:int(t+D)] = 1
+        return binary_vector
+    
+    def get_action_plan(self):
+        actions={}
+        starts=dict(zip(self.env.agents_id, [81,75,49,32,36,45,50][0:len(self.env.agents_id)]))
+        
+        for ag in self.env.agents_id:
+            agent=self.env.com.get_agent_obj(ag)
+            D=agent.apps[0].duration/self.env.tstep_size
+            actions[ag]=self.create_binary_vector(self.env.Tw,D,starts[ag])
+        
+        return actions
+    
 
             
 class DummyTester:
